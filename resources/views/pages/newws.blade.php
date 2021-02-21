be0908359944936f560223b04cc6b203645e7304
laybelmeyden <trycollens@gmail.com>
2021-02-21 16:08:59 +0300

    fixed/news

--- resources/views/pages/newws.blade.php
+++ resources/views/pages/newws.blade.php
@@ -11,7 +11,7 @@
                 <div class="data1">
                 <p class="p12">{{ $new->data }}</p>
                 </div>
-              @if(isEmpty($new->img))
+              @if($new->img  === isEmpty())
             <img src="assets/img/img5.png" class="carg">
             @else
               <img src="storage/{{ $new->img }}" class="carg">
