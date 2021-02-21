af986beb753c97e199900dc4fec47cd8574e70fd
laybelmeyden <trycollens@gmail.com>
2021-02-21 16:22:55 +0300

    fixed/news

--- resources/views/pages/newws.blade.php
+++ resources/views/pages/newws.blade.php
@@ -12,7 +12,7 @@
               <p class="p12">{{ $new->data }}</p>
             </div>
             
-            if($new-> img === status(404))
+            if($new-> img === 'errors::404')
             <img src="assets/img/img5.png" class="carg">
             @endif
           </div>
