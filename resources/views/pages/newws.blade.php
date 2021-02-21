496373d39319b4273cf23eb46095e09703e1ca40
laybelmeyden <trycollens@gmail.com>
2021-02-21 16:12:48 +0300

    fixed/news

--- resources/views/pages/newws.blade.php
+++ resources/views/pages/newws.blade.php
@@ -1,38 +1,38 @@
 @extends('layouts.master')
 
-@section('content') 
-            <section>
-        <div class="container">
-        <div class="row">
-        @foreach ($news as $new)
-        <div class="col l4 m12 s12">
+@section('content')
+<section>
+  <div class="container">
+    <div class="row">
+      @foreach ($news as $new)
+      <div class="col l4 m12 s12">
         <div class="card card1">
-            <div class="card-image">
-                <div class="data1">
-                <p class="p12">{{ $new->data }}</p>
-                </div>
-              @if($new->img -> count())
+          <div class="card-image">
+            <div class="data1">
+              <p class="p12">{{ $new->data }}</p>
+            </div>
+            @forelse ($news as $new)
+            <img src="storage/{{ $new->img }}" class="carg">
+            @empty
             <img src="assets/img/img5.png" class="carg">
-            @else
-              <img src="storage/{{ $new->img }}" class="carg">
-            @endif
-            </div>
-            <div class="card-content card-content1">
+            @endforelse
+          </div>
+          <div class="card-content card-content1">
             <div class="i1">
-            <p class="p10 ha1">{{ $new->titl }}</p>
+              <p class="p10 ha1">{{ $new->titl }}</p>
             </div>
-              <div class="ha2">
-                  <p class="p11">{!! $new->body !!}</p>
-              </div>
-            </div>
-            <div class="card-action act1">
-              <a class="a2" href="/new{{ $new->id }}">ПОДРОБНЕЕ</a>
+            <div class="ha2">
+              <p class="p11">{!! $new->body !!}</p>
             </div>
           </div>
+          <div class="card-action act1">
+            <a class="a2" href="/new{{ $new->id }}">ПОДРОБНЕЕ</a>
+          </div>
         </div>
-        @endforeach
-        </div>
-        </div>
-      </section>
-      
-      @endsection
+      </div>
+      @endforeach
+    </div>
+  </div>
+</section>
+
+@endsection
